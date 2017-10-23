5d1fbcaa86f16f09459387c9b53e0762cb0faf11
Stefan Jacholke <stefanjacholke@gmail.com>
2017-10-23 12:59:46 +0200

    Initial bot test

---
+++ bot.py
@@ -0,0 +1,59 @@
+from nicehash import nicehash
+import sys
+import ConfigParser
+
+
+def main():
+    if len(sys.argv) != 3:
+        print("python bot.py config.conf action")
+        # print("config file needs to contain the following lines")
+        print("action is can be one of the following")
+        print("\t cancel - cancels all active order")
+        print("\t start - inserts a new order at the top 5% of prices")
+        return
+    
+    conf_file = sys.argv[1]
+    action = sys.argv[2]
+
+    print("Reading from config file %s" % conf_file)
+    config = ConfigParser.ConfigParser()
+    config.read(conf_file)
+    print(config)
+    
+    apiid = config['API']['ApiId']
+    apikey = config['API']['ApiKey']
+    pool_host = config['POOL']['PoolHost']
+    pool_port = config['POOL']['PoolPort']
+    pool_user = config['POOL']['PoolUser']
+    pool_pass = config['POOL']['PoolPass']
+    algo = config['ORDER']['Algorithm']
+    location = config['ORDER']['Location']
+    amount = config['ORDER']['OrderAmount']
+    limit = config['ORDER']['OrderLimit']
+
+    print(apiid, apikey, pool_host, pool_port, pool_user, pool_pass, algo,
+            location, amount, limit)
+    
+    api = nicehash(apiid=apiid, apikey=apikey)
+    
+    def get_5_percentile_price(api, algo='Scrypt', location=0):
+        public_orders = api.get_public_orders(algo,location)['result']['orders']
+        prices = sorted([ float(order['price']) for order in public_orders if
+                float(order['workers']) > 2] )[::-1]
+        return prices[len(prices)/20]
+    
+
+    if action == "cancel":
+        print("Cancelling all active orders")
+        api.remove_all_orders(algo=algo, location=location)
+
+    if action == "start":
+        current_price = get_5_percentile_price(api)
+        print(current_price)
+        print("Price in the 10th percentile is currently %f" % current_price)
+        api.create_order(amount, price, limit, pool_host, pool_port, pool_user,
+          pool_pass, location=location,algo=algo)
+
+
+if __name__ == "__main__":
+    main()
